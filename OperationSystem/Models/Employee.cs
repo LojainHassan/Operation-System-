841a5acb17667171596f139783273eda069bd3ca
LojainHassan <lojainhteabat@gmail.com>
2023-12-01 19:12:58 +0300

    Add project files.

---
+++ OperationSystem/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace OperationSystem.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? City { get; set; }
+        public string? Email { get; set; }
+        public DateTime HireDate { get; set; }
+        public string? Mobile { get; set; }
+        public Gender Gender { get; set; }
+        public string? Position { get; set; }
+        public bool isActive { get; set; }
+        public bool isDeleted { get; set; }
+    }
+}
+
+/// <summary>
+/// Static List of Gender
+/// we dont need table for it
+/// </summary>
+/// 1 reference means where and how much is thing used , click on it you can see where it used 
+public enum Gender
+{
+    Male,Female
+}
